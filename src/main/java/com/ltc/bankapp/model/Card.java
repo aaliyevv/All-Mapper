b10f111cb4f1366d0fa56f5535e0d8d106bea38f
Ali <aaliyev14291@ada.edu.az>
2026-03-21 00:29:59 +0400

    Card

---
+++ src/main/java/com/ltc/bankapp/model/Card.java
@@ -0,0 +1,15 @@
+package com.ltc.bankapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
