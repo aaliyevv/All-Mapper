87411ee6f308277e43bf93ceea86117502998296
Ali <aaliyev14291@ada.edu.az>
2026-03-26 23:48:22 +0400

    Card Mapper

---
+++ src/main/java/com/ltc/bankapp/mapper/CardMapper.java
@@ -0,0 +1,29 @@
+package com.ltc.bankapp.mapper;
+
+import com.ltc.bankapp.dto.card.CardRequestDto;
+import com.ltc.bankapp.dto.card.CardResponseDto;
+import com.ltc.bankapp.model.Account;
+import com.ltc.bankapp.model.Card;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardMapper {
+
+    public Card toEntity(CardRequestDto cardRequestDto, Account account){
+
+        if (cardRequestDto == null || account == null){
+            return null;
+        }
+
+        Card card = new Card();
+
+        card.setCardNumber(cardRequestDto.getCardNumber());
+        card.setCardType(cardRequestDto.getCardType());
+        card.setExpirationDate(cardRequestDto.getExpirationDate());
+        card.setActive(true); // new card should be active by default
+        card.setAccount(account);
+
+        return card;
+    }
+
+    
