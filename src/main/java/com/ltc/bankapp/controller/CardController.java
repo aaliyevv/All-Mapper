7b3fe6f828387f1529da9877aedfca71eb83a927
Ali <aaliyev14291@ada.edu.az>
2026-04-09 09:30:50 +0400

    Card Controller

---
+++ src/main/java/com/ltc/bankapp/controller/CardController.java
@@ -0,0 +1,38 @@
+package com.ltc.bankapp.controller;
+
+import com.ltc.bankapp.dto.card.CardRequestDto;
+import com.ltc.bankapp.dto.card.CardResponseDto;
+import com.ltc.bankapp.model.Card;
+import com.ltc.bankapp.service.CardService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cards")
+@RequiredArgsConstructor
+public class CardController {
+
+    private final CardService cardService;
+
+    @PostMapping
+    public ResponseEntity<CardResponseDto> create(
+            @Valid @RequestBody CardRequestDto cardRequestDto) {
+
+        return ResponseEntity.ok(cardService.create(cardRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity <Page<CardResponseDto>> getAll(
+            @RequestBody(required = false) Long accountId, Pageable pageable) {
+
+        if (accountId != null) {
+            return ResponseEntity.ok(cardService.getByAccountId(accountId, pageable));
+        }
+
+        return ResponseEntity.ok(cardService.getAll(pageable));
+    }
